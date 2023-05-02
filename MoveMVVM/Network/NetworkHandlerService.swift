b99c6c5540c1c35ee6c1f1bdb3b15deea958d7ed
João Borges <joao.borges@bringglobal.com>
2023-05-03 00:34:14 +0100

    create generic network caller to use accross the app

--- MoveMVVM/Network/NetworkHandlerService.swift
+++ MoveMVVM/Network/NetworkHandlerService.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Alamofire
+
+
+protocol NetworkHandlerService {
+    func performRequest<Output: Decodable>(
+        type: Output.Type,
+        request: URLRequest,
+        completionHandler: @escaping (Result<Output, Error>) -> Void
+    )
+}
+
+class GenericApiCaller {
+    init() {}
+}
+
+extension GenericApiCaller: NetworkHandlerService {
+    func performRequest<Output: Decodable>(type: Output.Type, request: URLRequest, completionHandler: @escaping (Result<Output, Error>) -> Void) {
+        AF.request(request).responseDecodable(of: Output.self, decoder: JSONDecoder()) { response in
+            switch response.result {
+            case .success(let value):
+                completionHandler(.success(value))
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+
+    }
+}
