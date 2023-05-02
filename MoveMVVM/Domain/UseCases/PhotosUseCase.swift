8905bb8b4a137356f53fe418f120fec26922cd86
João Borges <joao.borges@bringglobal.com>
2023-05-03 00:35:34 +0100

    PhotoUseCase

--- MoveMVVM/Domain/UseCases/PhotosUseCase.swift
+++ MoveMVVM/Domain/UseCases/PhotosUseCase.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol PhotosUseCase {
+    func loadImagesList(completion: @escaping (Result<[Photo], Error>) -> Void)
+}
+
+class DefaultPhotosUseCase {
+    let repository: PhotosRepository
+
+    init(
+        photosRepository: PhotosRepository
+    ) {
+        self.repository = photosRepository
+    }
+}
+
+extension DefaultPhotosUseCase: PhotosUseCase {
+    func loadImagesList(completion: @escaping (Result<[Photo], Error>) -> Void) {
+        repository.loadImagesList(completion: completion)
+    }
+}
