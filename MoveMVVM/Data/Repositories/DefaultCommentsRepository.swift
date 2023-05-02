33f95fa65659c9e4883d530f654db1494553e682
João Borges <joao.borges@bringglobal.com>
2023-05-03 00:34:50 +0100

    comments repository

--- MoveMVVM/Data/Repositories/DefaultCommentsRepository.swift
+++ MoveMVVM/Data/Repositories/DefaultCommentsRepository.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Alamofire
+
+final class DefaultCommentsRepository {
+    private let networkHandler: NetworkHandlerService
+    
+    init(networkHandler: NetworkHandlerService) {
+        self.networkHandler = networkHandler
+    }
+}
+
+extension DefaultCommentsRepository: CommentsRepository {
+    func loadComments(for id: Int, completion: @escaping (Result<[Comment], Error>) -> Void) {
+        do {
+            let request = try CommentsRouter.get(id).asURLRequest()
+            networkHandler.performRequest(type: [CommentsResponseDTO].self,
+                                          request: request) { response in
+                switch response {
+                case .success(let value):
+                    completion(.success(value.map { $0.toComment() } ) )
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+}
