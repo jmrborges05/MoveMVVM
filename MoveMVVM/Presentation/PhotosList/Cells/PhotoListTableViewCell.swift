6e1878163a9b34a25ecf8ded43c0ec4ef0a8d8d1
João Borges <joao.borges@bringglobal.com>
2023-05-03 00:37:27 +0100

    table view cell for photo list setup constraints

--- MoveMVVM/Presentation/PhotosList/Cells/PhotoListTableViewCell.swift
+++ MoveMVVM/Presentation/PhotosList/Cells/PhotoListTableViewCell.swift
@@ -8,26 +8,56 @@
 
 import UIKit
 
-class PhotoListTableViewCellViewController: UITableViewCell {
-    static let reuseIdentifier = String(describing: PhotoListTableViewCellViewController.self)
-    var viewModel: PhotoListTableViewCellViewModel!
-    
-    
-    
-    class func create(with viewModel: PhotoListTableViewCellViewModel) -> PhotoListTableViewCellViewController {
-        let vc = PhotoListTableViewCellViewController.instantiateViewController()
-        vc.viewModel = viewModel
-        return vc
+class PhotoListTableViewCell: UITableViewCell {
+    static let reuseIdentifier = String(describing: PhotoListTableViewCell.self)
+    var viewModel: PhotoListTableViewCellViewModel? {
+        didSet {
+            titleLabel.text = viewModel?.title
+        }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var imageContainer: UIImageView = {
+        let container = UIImageView()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        return container
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        setupView()
+    }
 
-        bind(to: viewModel)
-        viewModel.viewDidLoad()
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     func bind(to viewModel: PhotoListTableViewCellViewModel) {
 
+    }
+    
+    func setupView() {
+        contentView.addSubview(imageContainer)
+        contentView.addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            imageContainer.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            imageContainer.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageContainer.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+        
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 12),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: 8),
+        ])
     }
 }
