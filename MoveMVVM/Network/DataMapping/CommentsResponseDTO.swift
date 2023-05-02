12c39bc78b7b97f2612daef24f02dff4e5c51b85
João Borges <joao.borges@bringglobal.com>
2023-05-03 00:35:55 +0100

    Comments reponse from server with mapping to comments ui object

--- MoveMVVM/Network/DataMapping/CommentsResponseDTO.swift
+++ MoveMVVM/Network/DataMapping/CommentsResponseDTO.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+//{
+//  "postId": 2,
+//  "id": 6,
+//  "name": "et fugit eligendi deleniti quidem qui sint nihil autem",
+//  "email": "Presley.Mueller@myrl.com",
+//  "body": "doloribus at sed quis culpa deserunt consectetur qui praesentium\naccusamus fugiat dicta\nvoluptatem rerum ut voluptate autem\nvoluptatem repellendus aspernatur dolorem in"
+//}
+
+struct CommentsResponseDTO: Decodable {
+    let id, postId: Int
+    let name, email, body: String
+}
+
+extension CommentsResponseDTO {
+    func toComment() -> Comment {
+        return .init(id: id,
+                     postId: postId,
+                     title: name,
+                     body: body,
+                     author: email)
+    }
+}
