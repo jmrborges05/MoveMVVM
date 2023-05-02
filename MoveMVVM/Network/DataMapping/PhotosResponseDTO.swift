0fa7d6250138af2be4d79142d551145728a4d053
João Borges <joao.borges@bringglobal.com>
2023-05-03 00:36:04 +0100

    Photos response from server object

--- MoveMVVM/Network/DataMapping/PhotosResponseDTO.swift
+++ MoveMVVM/Network/DataMapping/PhotosResponseDTO.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+//{
+//  "albumId": 100,
+//  "id": 4992,
+//  "title": "accusamus dolore enim quis necessitatibus voluptatibus",
+//  "url": "https://via.placeholder.com/600/de1377",
+//  "thumbnailUrl": "https://via.placeholder.com/150/de1377"
+//}
+
+
+struct PhotosResponseDTO: Decodable {
+    let id, albumId: Int
+    let title, url, thumbnailUrl: String
+}
+
+extension PhotosResponseDTO {
+    func toPhoto() -> Photo {
+        return .init(id: id,
+                     albumId: albumId,
+                     title: title,
+                     highResURL: url,
+                     lowResURL: thumbnailUrl)
+    }
+}
