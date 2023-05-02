5aaa4ee870952ae470c7cae7b0c804d0538b7240
João Borges <joao.borges@bringglobal.com>
2023-05-03 00:34:56 +0100

    photos repository

--- MoveMVVM/Data/Repositories/DefaultPhotosRepository.swift
+++ MoveMVVM/Data/Repositories/DefaultPhotosRepository.swift
@@ -1,8 +1,38 @@
 //
-//  DefaultImageRepository.swift
+//  DefaultPhotosRepository.swift
 //  MoveMVVM
 //
 //  Created by João Borges on 02/05/2023.
 //
 
 import Foundation
+import Alamofire
+
+final class DefaultPhotosRepository {
+    
+    private let networkHandler: NetworkHandlerService
+    
+    init(networkHandler: NetworkHandlerService) {
+        self.networkHandler = networkHandler
+    }
+}
+
+extension DefaultPhotosRepository: PhotosRepository {
+    func loadImagesList(completion: @escaping (Result<[Photo], Error>) -> Void) {
+        do {
+            let request = try PhotosRouter.get.asURLRequest()
+            networkHandler.performRequest(type: [PhotosResponseDTO].self,
+                                          request: request) { response in
+                switch response {
+                case .success(let value):
+                    completion(.success(value.map { $0.toPhoto() } ) )
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
+}
+
