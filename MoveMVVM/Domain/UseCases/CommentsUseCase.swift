520ad55d9bd961c6a29344e80c40b4fb54bb6a08
João Borges <joao.borges@bringglobal.com>
2023-05-03 00:35:41 +0100

    Comments Use case

--- MoveMVVM/Domain/UseCases/CommentsUseCase.swift
+++ MoveMVVM/Domain/UseCases/CommentsUseCase.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+protocol CommentsUseCase {
+    func loadComments(id: Int, completion: @escaping (Result<[Comment], Error>) -> Void)
+}
+
+class DefaultCommentsUseCase {
+    private let repository: CommentsRepository
+
+    init(
+        commentsRepository: CommentsRepository
+    ) {
+        self.repository = commentsRepository
+    }
+}
+
+extension DefaultCommentsUseCase: CommentsUseCase {
+    func loadComments(id: Int, completion: @escaping (Result<[Comment], Error>) -> Void) {
+        repository.loadComments(for: id, completion: completion)
+    }
+}
