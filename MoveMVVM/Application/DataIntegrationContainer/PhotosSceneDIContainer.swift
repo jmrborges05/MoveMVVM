185f374f5f7d7a443a2265c3989fd48ea5645a70
João Borges <joao.borges@bringglobal.com>
2023-05-03 00:34:33 +0100

    implement constructors for use cases and repositories

--- MoveMVVM/Application/DataIntegrationContainer/PhotosSceneDIContainer.swift
+++ MoveMVVM/Application/DataIntegrationContainer/PhotosSceneDIContainer.swift
@@ -17,9 +17,23 @@ protocol PhotosFlowCordinatorConstructors {
     func createPhotosListViewModel(actions:PhotosListViewModelActions) -> PhotosListViewModel
     func createPhotoDetailViewModel(photo: Photo) -> PhotoDetailViewModel
     func createPhotosFlowCoordinator(navigationController: UINavigationController) -> PhotosFlowCoordinator
+    
+    // photos use case and repository
+    func createPhotosUseCase() -> PhotosUseCase
+    func createPhotosRepository() -> PhotosRepository
+    // comments use case and repository
+    func createCommentsUseCase() -> CommentsUseCase
+    func createCommentsRepository() -> CommentsRepository
 }
 
 class PhotosSceneDIContainer: PhotosFlowCoordinatorDependencies {
+    
+    private var networkCaller: NetworkHandlerService
+    
+    init(networkCaller: NetworkHandlerService) {
+        self.networkCaller = networkCaller
+    }
+    
     func createPhotosListViewController(actions:PhotosListViewModelActions) -> PhotosListViewController {
         PhotosListViewController.create(
             with: createPhotosListViewModel(actions: actions)
@@ -34,12 +48,30 @@ class PhotosSceneDIContainer: PhotosFlowCoordinatorDependencies {
 }
 
 extension PhotosSceneDIContainer: PhotosFlowCordinatorConstructors {
+    func createCommentsUseCase() -> CommentsUseCase {
+        DefaultCommentsUseCase(commentsRepository: createCommentsRepository())
+    }
+    
+    func createCommentsRepository() -> CommentsRepository {
+        DefaultCommentsRepository(networkHandler: networkCaller)
+    }
+    
+    func createPhotosRepository() -> PhotosRepository {
+        DefaultPhotosRepository(networkHandler: networkCaller)
+    }
+    
+    func createPhotosUseCase() -> PhotosUseCase {
+        DefaultPhotosUseCase(photosRepository: createPhotosRepository())
+    }
+    
     func createPhotosListViewModel(actions:PhotosListViewModelActions) -> PhotosListViewModel {
-        DefaultPhotosListViewModel(actions: actions)
+        DefaultPhotosListViewModel(actions: actions,
+                                   useCase: createPhotosUseCase())
     }
     
     func createPhotoDetailViewModel(photo:Photo) -> PhotoDetailViewModel {
-        DefaultPhotoDetailViewModel(photo: photo)
+        DefaultPhotoDetailViewModel(photo: photo,
+                                    commentsUseCase: createCommentsUseCase())
     }
     
     func createPhotosFlowCoordinator(navigationController: UINavigationController) -> PhotosFlowCoordinator {
