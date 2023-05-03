d2bace40e8d958a01c91af530c48cc0907216989
João Borges <joao.borges@bringglobal.com>
2023-05-03 01:32:58 +0100

    comments table view cell setup from viewmodel

--- MoveMVVM/Presentation/PhotoDetail/CommentsCell/CommentsTableViewCell.swift
+++ MoveMVVM/Presentation/PhotoDetail/CommentsCell/CommentsTableViewCell.swift
@@ -6,3 +6,82 @@
 //
 
 import UIKit
+import Kingfisher
+
+class CommentsTableViewCell: UITableViewCell {
+    static let reuseIdentifier = String(describing: CommentsTableViewCell.self)
+    var viewModel: CommentsTableViewCellViewModel? {
+        didSet {
+            titleLabel.text = viewModel?.title
+            bodyLabel.text = viewModel?.body
+            authorLabel.text = viewModel?.author
+        }
+    }
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var authorLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var bodyLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        selectionStyle = .none
+        setupView()
+    }
+
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        titleLabel.text = nil
+        bodyLabel.text = nil
+        authorLabel.text = nil
+    }
+    
+    func bind(to viewModel: PhotoListTableViewCellViewModel) {}
+    
+    func setupView() {
+
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(bodyLabel)
+        contentView.addSubview(authorLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+        
+            bodyLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4),
+            bodyLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            bodyLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            
+            authorLabel.topAnchor.constraint(equalTo: bodyLabel.bottomAnchor, constant: 4),
+            authorLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            authorLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            authorLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+    }
+}
