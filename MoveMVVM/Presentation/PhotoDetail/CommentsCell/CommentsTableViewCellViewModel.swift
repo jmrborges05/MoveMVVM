a227ba91cacbf3cb682cd964dcc4c01c29764184
João Borges <joao.borges@bringglobal.com>
2023-05-03 01:33:09 +0100

    view model to feed cell for comments

--- MoveMVVM/Presentation/PhotoDetail/CommentsCell/CommentsTableViewCellViewModel.swift
+++ MoveMVVM/Presentation/PhotoDetail/CommentsCell/CommentsTableViewCellViewModel.swift
@@ -1,8 +1,44 @@
 //
-//  CommentsTableViewModel.swift
+//  CommentsTableViewCellViewModel.swift
 //  MoveMVVM
 //
 //  Created by João Borges on 03/05/2023.
 //
 
 import Foundation
+
+protocol CommentsTableViewCellViewModelInput {
+}
+
+protocol CommentsTableViewCellViewModelOutput {
+    var title: String { get }
+    var author: String { get }
+    var body: String { get }
+}
+
+protocol CommentsTableViewCellViewModel: CommentsTableViewCellViewModelInput, CommentsTableViewCellViewModelOutput { }
+
+class DefaultCommentsTableViewCellViewModel: CommentsTableViewCellViewModel {
+    
+    private var item:Comment
+    
+    var title: String {
+        item.title
+    }
+    
+    var author: String {
+        "Author: \(item.author)"
+    }
+    
+    var body: String {
+        item.body
+    }
+    
+    init(comment:Comment) {
+        self.item = comment
+    }
+}
+
+// MARK: - INPUT. View event methods
+extension DefaultCommentsTableViewCellViewModel {}
+
